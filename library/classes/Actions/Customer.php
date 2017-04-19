6773c0d705a2a5887a2ade12aeb776ed02614378
Nati <lolz@brainpal.io>
2017-04-19 12:00:06 +0300

    Changed ActionUser to ActionCustomer

--- library/classes/Actions/Customer.php
+++ library/classes/Actions/Customer.php
@@ -1,12 +1,12 @@
 <?php
 
 /** 
- * This is the user category controller.
- * This controller handles everything user related, ie. login in, profile viewing, etc.
+ * This is the customer controller.
+ * This controller handles everything customer related, ie. login in, profile viewing, etc.
  * 
  * @author Nati
  */
-class ActionsUser extends Actions {
+class ActionsCustomer extends Actions {
     
     protected $actions = array(
         'index' => false,
@@ -16,62 +16,51 @@ class ActionsUser extends Actions {
         'getclientsamount' => true,
         'getconvertedclientsamount' => true,
     );
-    
-    /**
-     * Constructs the ActionUser instance and initialize its properties.
-     * Declares that this controller DOES NOT require authentication.
-     * 
-	 * @param BrainPal $app_object A reference to the application object.
-	 */
-	public function __construct(&$app_object) {
-        // Call the main Action constructor
-        parent::__construct($app_object);
-	}
 	
 	/**
 	 * This is the function that's being called when the user doesn't enter any method.
 	 * It executes the default functionality for the controller, so in this case
-	 * it displays the profile page for a logged in user,
+	 * it displays the profile page for a logged in customer,
 	 * or redirects to a login page for an unauthenticated one.
 	 */
 	public function index() {
-		// Check if the user is logged in
+		// Check if the customer is logged in
 		if( $this->app->user->authenticated ) {
 			// He is! Send back the authentication flag
 		    $this->app->output->setArguments(array('auth' => true));
 		}
 		else {
-		    // Inform the front-end the user is not authenticated
+		    // Inform the front-end the customer is not authenticated
 		    $this->app->output->setArguments(array('auth' => false));
 		}
 	}
 	
 	/**
-	 * Displays the complete profile of the specified user.
-	 * If no user is specified, display the profile of the currently logged in user.
+	 * Displays the complete profile of the specified customer.
+	 * If no customer is specified, display the profile of the currently logged in customer.
 	 * 
-	 * If the user is not authenticated, redirect to the login page.
+	 * If the customer is not authenticated, redirect to the login page.
 	 */
 	public function profile() {	    
-	    // @TODO Load the profile page with the user values
+	    // @TODO Load the profile page with the customer values
 	}
 	
 	/**
-	 * Get/set the basic information for the current user (username and password).
+	 * Get/set the basic information for the current customer (username and password).
 	 */
 	public function basicInfo() {
 		// He is, Check if the form has been submitted
 		if( !empty($this->app->input('post')) ) {
-			// Check that the user has entered his correct password
+			// Check that the customer has entered his correct password
 			if( $this->app->input('post', 'verify_password') === $this->app->user->password ) {
-				// If the user is changing his password, check that it matches
+				// If the customer is changing his password, check that it matches
 				if( !empty($this->app->input('post', 'password1')) &&
 					($this->app->input('post', 'password1') === $this->app->input('post', 'password2')) ) {
-					// It does, Set the basic information of the user
+					// It does, Set the basic information of the customer
 					$this->app->user->username = $this->app->input('post', 'username');
 					$this->app->user->password = $this->app->input('post', 'password1');
 					
-					// Save the additional information of the user in the DB
+					// Save the additional information of the customer in the DB
 					$this->app->user->save(USER_SAVE_BASIC);
 					
 					// Return that the saving process succeeded
@@ -79,27 +68,27 @@ class ActionsUser extends Actions {
 				}
 				else {
 					// It doesn't, inform the user
-					error('Passwords Doesn\'t match at User::basicInfo()');
+					error('Passwords Doesn\'t match at Customer::basicInfo()');
 				}					
 			}
 			else {
 				// He hasn't, terminate with an error
-				error('Wrong password! at User::basicInfo()');
+				error('Wrong password! at Customer::basicInfo()');
 			}
 		}
 		else {
-			// Returns the user basic information
+			// Returns the customer basic information
 			// @TODO Add the template arguments
 		}
 	}
 	
 	/**
-	 * Get/set the additional information for the current user (full name, address, phone number, etc.).
+	 * Get/set the additional information for the current customer (full name, address, phone number, etc.).
 	 */
 	public function additionalInfo() {
-		// He is, Check if the form has been submitted
+		// Check if the form has been submitted
 		if( !empty($this->app->input('post')) ) {
-			// Set the additional information of the user
+			// Set the additional information of the customer
 			$this->app->user->first_name = $this->app->input('post', 'first_name');
 			$this->app->user->last_name = $this->app->input('post', 'last_name');
 			$this->app->user->gender = ($this->app->input('post', 'gender') === 'Male') ? '0' : '1'; // Gender (0 = male, 1 = female)
@@ -109,7 +98,7 @@ class ActionsUser extends Actions {
 			
 			// @TODO Add support for profile pictures
 			
-			// Save the additional information of the user in the DB
+			// Save the additional information of the customer in the DB
 			$this->app->user->save(USER_SAVE_ADDITIONAL);
 			
 			// Return that the registration process succeeded
@@ -122,7 +111,7 @@ class ActionsUser extends Actions {
 	}
 	
 	/**
-	 * Retrieves the amount of clients this specific user handled.
+	 * Retrieves the amount of clients this specific customer handled.
 	 */
 	public function getClientsAmount() {
 	    /* Retrieve information from the request */
@@ -143,7 +132,7 @@ class ActionsUser extends Actions {
 	        error($error);
 	    }
 	
-	    // Get the amount of unique clients for that user
+	    // Get the amount of unique clients for that customer
 	    $clients = $this->app->user->getClients(false, $days, DB_MAX_LIMIT);
 	
 	    // If we've reached here, everything is OK. Return the clients amount
@@ -154,7 +143,7 @@ class ActionsUser extends Actions {
 	}
 	
 	/**
-	 * Retrieves the amount of clients this specific user handled.
+	 * Retrieves the amount of clients this specific customer handled.
 	 */
 	public function getConvertedClientsAmount() {
 	    /* Retrieve information from the request */
@@ -175,7 +164,7 @@ class ActionsUser extends Actions {
 	        error($error);
 	    }
 	
-	    // Get the amount of unique clients for that user
+	    // Get the amount of unique clients for that customer
 	    $clients = $this->app->user->getConvertedClients(false, $days, DB_MAX_LIMIT);
 	
 	    // If we've reached here, everything is OK. Return the clients amount
